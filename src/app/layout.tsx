6d41f67991d806255fa76f6c17f7a5458a8d4c2b
kodaneflash <jamesonmarcher@gmail.com>
2024-04-13 17:56:02 -0400

    modified:   src/assets/logo.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,49 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "./ThemeProvider";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FlowBrain",
-  description: "The intelligent note-taking app",
+  title: "Syncade",
+  /**
+   * Provides a brief description of the Syncade application, which is an intelligent note-taking app powered by AI.
+   */
+  description: "Intelligent note-taking app, powered by AI.",
+  openGraph: {
+    title: "Syncade",
+    description: "Intelligent note-taking app, powered by AI.",
+    images: [
+      {
+        url: "https://www.yoursite.com/og-image.jpg",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    url: "https://www.syncade.io",
+    type: "website",
+    siteName: "Syncade",
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@yourtwitterhandle",
+    title: "Syncade",
+    description: "Intelligent note-taking app, powered by AI.",
+    images: ["https://www.yoursite.com/twitter-image.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  alternates: {
+    canonical: "https://www.syncade.io",
+    languages: {
+      "en-US": "https://syncade.iom",
+      es: "https://www.syncade.io/es",
+    },
+  },
 };
 
 export default function RootLayout({
